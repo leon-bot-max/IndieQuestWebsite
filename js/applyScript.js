e77918a4784f107c5147e2b9700eb5fc664e574e
leon-bot-max <61838582+leon-bot-max@users.noreply.github.com>
2021-05-27 19:53:41 +0200

    apply validation

--- js/applyScript.js
+++ js/applyScript.js
@@ -1,9 +1,12 @@
 var submitButton;
+
+let inputFormatPnr = new RegExp("[0-9]{8}-[0-9]{4}"); //Expected format
+
+var personnummerInput;
 var otherCountryRadio, swedenRadio;
 var otherCountryInput, otherCountryLabel;
 
 function init() {
-    submitButton = document.getElementById("submitButton");
 
     otherCountryRadio = document.getElementById("annatLand");
     swedenRadio = document.getElementById("svensk");
@@ -12,11 +15,21 @@ function init() {
     swedenRadio.onclick = countryButtonClick;
     otherCountryRadio.onclick = countryButtonClick;
 
-    submitButton.onclick = validateRequiredInputs;
+    personnummerInput = document.getElementById("personnummer");
+    personnummerInput.onblur = personnummerFormat;
+    personnummerInput.onchange = personnummerFormat;
 
 
 }
 window.onload = init;
+
+function personnummerFormat() { //Remove all non digits and Autoformat if they forget "-"
+    if (personnummerInput.value.length >= 12 && !inputFormatPnr.test(personnummerInput.value)) {
+        personnummerInput.value = personnummerInput.value.replace(/[^\d]/g, '');
+        personnummerInput.value = personnummerInput.value.slice(0, 8) + "-" + personnummerInput.value.slice(8, 12);//insert '-'
+        
+    }
+}
 
 function countryButtonClick() {
     if (otherCountryRadio.checked) { //other country, make text input visible
@@ -30,14 +43,56 @@ function countryButtonClick() {
 }
 
 function validateRequiredInputs() {
-    if (!validatePersonnummer(document.getElementById("personnummer").value)){
-        alert("Invalid personnummer");
+    var reason = "";
+
+    if (!validatePersonnummer(document.getElementById("personnummer").value)) {
+        reason += "Invalid personnummer\n";
     }
+
+    if (!validateTextInput(document.getElementById("fname"))){
+        reason += "Invalid firstname\n";
+    }
+    if (!validateTextInput(document.getElementById("lname"))){
+        reason += "Invalid lastname\n";
+    }
+    if (!validateTextInput(document.getElementById("adress"))){
+        reason += "Invalid address\n";
+    }
+
+    if (otherCountryRadio.checked) {
+        if (!validateTextInput(otherCountryInput)){
+            reason += "Invalid other country";
+        }
+    }
+    trimValue(document.getElementById("co"));
+
+    if (reason == ""){//No invalid inputs
+        alert("All inputs accepted")
+        return true;
+    }
+    alert(reason);
+    return false;
+}
+function validateTextInput(textField)
+{
+    //All names valid as long as not empty, trimmed so they don't begin or end with blank
+    if (!textField){
+        return false;
+    }
+    trimValue(textField);
+    
+    if (textField.length <= 0){
+        return false;
+    }
+    return true;
+}
+
+function trimValue(textField) {
+    textField.value = textField.value.trim();
 }
 
 function validatePersonnummer(input) { //input format: ÅÅÅÅMMDD-XXXX
-
-    if (!input || input.length != 13) //Wrong format
+    if (!input || !inputFormatPnr.test(input)) //Wrong format
     {
         return false;
     }
@@ -62,7 +117,7 @@ function validatePersonnummer(input) { //input format: ÅÅÅÅMMDD-XXXX
     }
 
 
-    //check psn
+    //check pnr
 
     input = input.slice(2, 8) + input.slice(9, 13); //remove the "-" format: ÅÅMMDDXXXX
 
