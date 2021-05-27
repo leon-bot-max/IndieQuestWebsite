22d7830af2adca93e721784f86163626d60a921b
leon-bot-max <61838582+leon-bot-max@users.noreply.github.com>
2021-05-27 14:27:40 +0200

    text input if not swedish

--- js/applyScript.js
+++ js/applyScript.js
@@ -1,4 +1,36 @@
+var submitButton;
+var otherCountryRadio, swedenRadio;
+var otherCountryInput, otherCountryLabel;
 
+function init(){
+	submitButton = document.getElementById("submitButton");
+    
+    otherCountryRadio = document.getElementById("annatLand");
+    swedenRadio = document.getElementById("svensk");
+    otherCountryInput = document.getElementById("land");
+
+    swedenRadio.onclick = countryButtonClick;
+    otherCountryRadio.onclick = countryButtonClick;
+
+
+	
+}
+window.onload = init;
+
+function countryButtonClick(){
+    if (otherCountryRadio.checked){ //other country, make text input visible
+        otherCountryInput.style.visibility = "visible"
+        otherCountryInput.required = true;
+    }
+    else{
+        otherCountryInput.style.visibility = "hidden"
+        otherCountryInput.required = false;
+    }
+}
+
+function validateRequiredInputs(){
+
+}
 
 
 
